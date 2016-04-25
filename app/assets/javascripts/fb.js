9a351444542926e86a72769afb9170bb50bb9135
Andrey Shmatko <shmatuan@gmail.com>
2016-04-25 11:34:48 +0300

    FBlogin()

--- app/assets/javascripts/fb.js
+++ app/assets/javascripts/fb.js
@@ -1,44 +1,34 @@
-function statusChangeCallback(response) {
-    if (response.status === 'connected') {
-        testAPI();
-    } else if (response.status === 'not_authorized') { // The person is logged into Facebook, but not your app.
-        console.log('Please log into this app.');
-    } else {
-        console.log('Please log into Facebook.'); // The person is not logged into Facebook, so we're not sure if they are logged into this app or not.
-    }
-}
-
-function checkLoginState() {
-    FB.getLoginStatus(function(response) {
-        statusChangeCallback(response);
-    });
-}
-
-window.fbAsyncInit = function() {
+function FBlogin() {
     FB.init({
         appId      : '1997533793805209',
+        //secret     : 'ed28000574a370346a5e6545444a0ae9',
         cookie     : true,  // enable cookies to allow the server to access the session
         xfbml      : true,  // parse social plugins on this page
         version    : 'v2.2' // use version 2.2
     });
 
     FB.getLoginStatus(function(response) {
-        statusChangeCallback(response);
+        if (response.status === 'connected') {
+            login();
+        }
+        else {
+            FB.login(function() {
+                login();
+            }, {scope: 'public_profile,email'});
+        }
     });
-};
+}
 
-(function(d, s, id) { // Load the SDK asynchronously
+function login() {
+    FB.api('/me', {fields: 'email,name,first_name,last_name,birthday,age_range,bio,gender,picture,link,locale'}, function(response) {
+        console.log(response);
+    });
+}
+
+(function(d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
     js = d.createElement(s); js.id = id;
     js.src = "//connect.facebook.net/en_US/sdk.js";
     fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
-function testAPI() {
-    console.log('Welcome!  Fetching your information.... ');
-
-    FB.api('/me', {fields: 'email,name,birthday,gender,picture'}, function(response) {
-        console.log(response);
-    });
-}
+}(document, 'script', 'facebook-jssdk')); // Load the SDK asynchronously
